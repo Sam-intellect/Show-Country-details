c6280de5f2e0858c054114918d6c57dfdb668f0c
Sam-intellect <alukoolakunlesamuel@gmail.com>
2022-09-28 16:26:01 +0100

    getting data from API

--- src/js/index.js
+++ src/js/index.js
@@ -6,15 +6,15 @@ const countriesContainer = document.querySelector('.countries');
 const renderCountry = function (data, className = '') {
   const html = `
   <article class="country ${className}">
-    <img class="country__img" src="${data.flag}" />
+    <img class="country__img" src="${data.flags.svg}" />
     <div class="country__data">
-      <h3 class="country__name">${data.name}</h3>
+      <h3 class="country__name">${data.name.common}</h3>
       <h4 class="country__region">${data.region}</h4>
       <p class="country__row"><span>👫</span>${(
         +data.population / 1000000
       ).toFixed(1)} people</p>
-      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[1]}</p>
+      <p class="country__row"><span>💰</span>${data.currencies}</p>
     </div>
   </article>
   `;
@@ -24,15 +24,15 @@ const renderCountry = function (data, className = '') {
 
 const showCountryDetails = async function (country) {
   try {
-    const request = await fetch(
+    const response = await fetch(
       `https://restcountries.com/v3.1/name/${country}`
     );
-    const data = await request.json();
-    console.log(data);
-    renderCountry(data);
+    const data = await response.json();
+    renderCountry(data[0]);
+    console.log(data[0]);
   } catch (err) {
-    console.log(err);
+    console.error(err);
   }
 };
-showCountryDetails('Nigeria');
+showCountryDetails('nigeria');
 console.log('FIRST');

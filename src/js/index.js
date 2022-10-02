0f9fb540e0fa55737fe553a670899038fbd91ff2
Sam-intellect <alukoolakunlesamuel@gmail.com>
2022-10-02 17:34:13 +0100

    fixing location

--- src/js/index.js
+++ src/js/index.js
@@ -44,9 +44,28 @@ const showCountryDetails = async function (country) {
       lang.innerHTML = `<span>🗣️</span>${value}`;
     });
 
+    console.log(data[0].latlng);
+
     console.log(data);
   } catch (err) {
     console.error(err);
   }
 };
 showCountryDetails('USA');
+
+// GO FIND me
+function success(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  console.log(latitude, longitude);
+}
+
+function error() {
+  console.error('Unable to retrieve your location');
+}
+
+if (!navigator.geolocation) {
+  console.log('Geolocation is not supported by your browser');
+} else {
+  navigator.geolocation.getCurrentPosition(success, error);
+}
